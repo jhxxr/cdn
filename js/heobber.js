5eb2f6d2ebcf788c30d4a9daca8074d666ece437
jhxxr <89771592+jhxxr@users.noreply.github.com>
2022-05-05 11:16:25 +0800

    Update heobber.js

--- js/heobber.js
+++ js/heobber.js
@@ -1,3 +1,4 @@
+// 哔哔页面
 // if (document.querySelector('#bber-loading')) {
 //   var loading_pic = document.getElementById('bber-loading');
 //   loading_pic.innerHTML = '<span id="moments_loading"><i class="fa fa-spinner fa-spin"></i></span>';
@@ -81,7 +82,7 @@ function urlToLink(str) {
 var re =/\bhttps?:\/\/(?!\S+(?:jpe?g|png|bmp|gif|webp|gif))\S+/g;
 var re_forpic =/\bhttps?:[^:<>"]*\/([^:<>"]*)(\.(jpeg)|(png)|(jpg)|(webp))(!blogimg)?/g;
 str =str.replace(re_forpic,function (imgurl) {
-  return '<a href="' + imgurl + '"><img src="' + imgurl + '" /></a>';
+  return '<a href="' + imgurl + '"><img src="' + imgurl + '" width="100%"/></a>';
 });
 str =str.replace(re,function (website) {
   return " <a href='" + website + "'rel='noopener' target='_blank'>↘链接↙</a> ";
